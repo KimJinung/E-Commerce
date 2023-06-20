fe25874aabc86b26f0c347821008bfde9849bee5
KimJinung <KimJinung@outlook.com>
2023-06-20 21:17:38 +0900

    Add fields

--- src/main/java/kimjinung/ecommerce/dto/item/ItemRegistrationRequestDto.java
+++ src/main/java/kimjinung/ecommerce/dto/item/ItemRegistrationRequestDto.java
@@ -1,4 +1,15 @@
 package kimjinung.ecommerce.dto.item;
 
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class ItemRegistrationRequestDto {
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+    private List<Integer> categories;
+
 }
--- src/main/java/kimjinung/ecommerce/dto/order/OrderRequestDto.java
+++ src/main/java/kimjinung/ecommerce/dto/order/OrderRequestDto.java
@@ -1,4 +1,16 @@
 package kimjinung.ecommerce.dto.order;
 
+import lombok.Data;
+
+import javax.print.attribute.IntegerSyntax;
+import java.util.Map;
+
+@Data
 public class OrderRequestDto {
+
+    private String userId;
+    private Map<String, Integer> itemCart;
+    private String city;
+    private String street;
+    private String zipCode;
 }
