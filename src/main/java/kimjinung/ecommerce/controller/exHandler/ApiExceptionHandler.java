b13de15f3ed7d902d31a20e139b10b8c02e769cf
KimJinung <KimJinung@outlook.com>
2023-06-21 18:07:24 +0900

    Chore change status code

--- src/main/java/kimjinung/ecommerce/controller/exHandler/ApiExceptionHandler.java
+++ src/main/java/kimjinung/ecommerce/controller/exHandler/ApiExceptionHandler.java
@@ -22,37 +22,37 @@ public class ApiExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(RuntimeException.class)
     public ResponseDto<ErrorResult> itemRegisterFailException(RuntimeException ex) {
-        return baseException(500, Error.INVALID_REQUEST, ex);
+        return baseException(500, Error.SERVER_ERROR, ex);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(NotEnoughItemStockException.class)
     public ResponseDto<ErrorResult> itemRegisterFailException(NotEnoughItemStockException ex) {
-        return baseException(500, Error.INVALID_REQUEST, ex);
+        return baseException(400, Error.INVALID_REQUEST, ex);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(ItemNotFoundException.class)
     public ResponseDto<ErrorResult> itemRegisterFailException(ItemNotFoundException ex) {
-        return baseException(500, Error.INVALID_REQUEST, ex);
+        return baseException(400, Error.INVALID_REQUEST, ex);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(CategoryNotFoundException.class)
     public ResponseDto<ErrorResult> itemRegisterFailException(CategoryNotFoundException ex) {
-        return baseException(500, Error.INVALID_REQUEST, ex);
+        return baseException(400, Error.INVALID_REQUEST, ex);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(AlreadyExistCategoryNameException.class)
     public ResponseDto<ErrorResult> itemRegisterFailException(AlreadyExistCategoryNameException ex) {
-        return baseException(500, Error.INVALID_REQUEST, ex);
+        return baseException(400, Error.INVALID_REQUEST, ex);
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(CategoryMaxDepthExceedException.class)
     public ResponseDto<ErrorResult> itemRegisterFailException(CategoryMaxDepthExceedException ex) {
-        return baseException(500, Error.INVALID_REQUEST, ex);
+        return baseException(400, Error.INVALID_REQUEST, ex);
     }
 
     private ResponseDto<ErrorResult> baseException(int statusCode, Error error, RuntimeException exception) {
